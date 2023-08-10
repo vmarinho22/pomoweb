5150e498008d741048c9b4cac8bbf1a53bd701db
Vinicius Marinho <viniciusmarinho1616@gmail.com>
2023-08-10 18:03:14 -0300

    feat: adding test system

---
+++ jest.config.js
@@ -0,0 +1,15 @@
+/** @type {import('ts-jest').JestConfigWithTsJest} */
+module.exports = {
+  preset: 'ts-jest',
+  moduleNameMapper: {
+    '@/(.*)$': '<rootDir>/src/$1',
+    "\\.(jpg|ico|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$": "<rootDir>/tests/imageMock.ts",
+  },
+  transform: {
+    '^.+\\.ts$': 'ts-jest',
+  },
+  setupFilesAfterEnv: ['./tests/setupTests.ts'],
+  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
+  modulePaths: ['<rootDir>'],
+  testEnvironment: 'jsdom',
+};
